4288ec91d00b8471a55514ad89818bc56eaaf9f9
Mirajul Islam Mahim <30780836+AKmahim@users.noreply.github.com>
2023-07-01 11:43:27 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ function handleOrientation(event) {
   var alpha = event.alpha; // The compass direction in degrees
   var compass = document.querySelector('.compass');
   var needle = document.querySelector('.needle');
-  
+
   compass.style.transform = 'rotate(' + alpha + 'deg)';
   needle.style.transform = 'rotate(' + -alpha + 'deg)';
+
+  var direction = getDirection(alpha);
+  console.log('Current direction:', direction);
+}
+
+function getDirection(alpha) {
+  var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+  var index = Math.round(alpha / 45) % 8;
+  return directions[index];
 }
